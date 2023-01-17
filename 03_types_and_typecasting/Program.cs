b2121c775b5968e77df85ee5718895f94b96024d
chnnxyz <s@chnn.xyz>
2023-01-17 10:04:15 -0600

    types and typecasting

---
+++ 03_types_and_typecasting/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TypeCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables can be implicitly casted in this hierarchy
+            // char -> int -> long -> float -> double -> string
+            // an exmple of implicit casting is 
+            int myInt = 9;
+            double myDouble = myInt;
+
+            // Explicit casting
+            // use type between parentheses
+            // This can be used when not following the hierarchy above
+            double myDouble2 = 9.78;
+            int myInt2 = (int) myDouble2;    // Manual casting: double to int
+
+            Console.WriteLine(myDouble2);   // Outputs 9.78
+            Console.WriteLine(myInt2);      // Outputs 9, essentially truncates
+
+            // Aditionally, the system namespace has a Convert class with
+            // several conversion methods
+
+            int myInt3 = 10;
+            double myDouble3 = 5.25;
+            bool myBool3 = true;
+
+            Console.WriteLine(Convert.ToString(myInt3));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt3));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble3));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool3));   // convert bool to string
+
+        }
+    }
+}
---
+++ 04_user_input/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UserIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Think of readline as equivalent to writeline but for the user
+            // contrary to python you cannot add a direct prompt
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            // Let's try typecasting in one line
+            Console.WriteLine("What is your age?");
+            // The following typecast for some reason dont work,
+            // probably because on compile time you cant guarantee this string
+            // will be parsable to int.
+            // int age = (int) Console.ReadLine();
+
+            // as expected, this function works at runtime so it will crash
+            // the program if the string provided cant be converted
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You are " + name);
+            Console.WriteLine("You are " + age + " years old");
+            Console.WriteLine("You will be " + (age + 1) + " years old");
+        }
+    }
+}
