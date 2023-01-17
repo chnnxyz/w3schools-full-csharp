a3b2a8fb979c801623396b73bfbde047d5ff0787
chnnxyz <s@chnn.xyz>
2023-01-17 09:33:19 -0600

    some kindergarten stuff

---
+++ 01_hello_world/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using keyword is basically import equivalent
+using System;
+
+// a namespace organizes a set of classes and can contain other namespaces
+namespace HelloWorld
+{
+    class Program
+    {
+        // the Main method always has to be TitleCased
+        static void Main(string[] args)
+        {
+            // Console is included within System.
+            // using System is equivalent in python to from system import *
+
+            // Writeline adds \n at the end of the text, write does not.
+            // String needs to always use double quotes, single quotes are char
+            // variables
+            Console.WriteLine("Hello World!");
+            Console.Write("Hi");
+            Console.Write(" ");
+            Console.WriteLine("baby!");
+        }
+    }
+}
---
+++ 02_vars_and_dtypes/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace VarsAndTypes
+{
+    class Program {
+        static void Main(string[] args){
+            // To declare a variable, you pass in a data type and then varname
+            // then assign
+            int someInteger = 5;
+            // double is the standard for floating point variables
+            double someFloat = 5.99;
+            // Can you add int and float?
+            Console.WriteLine("Integer + float test:");
+            Console.WriteLine(someFloat + someInteger); 
+            // char is a primitive that can only contain one character
+            char a = 'a';
+            // string on the other side, is essentially an array of chars
+            string myName = "Sasha";
+            // bool is short for boolean
+            // true and false are not capitalized.
+            bool isItTrue = false;
+
+            // Contrary to rust, all variables are mutable by default
+            // to make them immutable, it is needed to add const before.
+            const double gravity = 9.81;
+
+            // Most variables have a string form and can be concatenated with
+            // the + operator
+            Console.WriteLine("Most variables can be parsed as text:");
+            Console.WriteLine("Here is a character: " + a);
+            Console.WriteLine("Here is a string: " + myName);
+            Console.WriteLine("Here is a bool: " + isItTrue);
+            Console.WriteLine("Here is a the constant of gravity: " + gravity);
+            
+            // You can assign several variables on a single line.
+            int x = 5, y = 6, z = 50;
+            Console.WriteLine(x + y + z);
+
+            // You can also instantiate and then assign.
+            int o, p, q;
+            o = p = q = 100;
+            Console.WriteLine(o+p+q);
+
+        }
+    }
+}
